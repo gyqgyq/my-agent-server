481270774f0d6c32a11b3db6fc3b366f9a987872
gaoyongqiang <gaoyongqiang@jiandanxinli.com>
2026-05-10 19:19:01 +0800

    feat: 中间件

--- middleware.py
+++ middleware.py
@@ -4,10 +4,16 @@ import time
 
 def my_middleware(app: FastAPI):
     @app.middleware("http")
-    async def count_time(request: Request, call_next):
+    async def count_time_middleware(request: Request, call_next):
         start_time = time.time()
         response = await call_next(request)
         end_time = time.time()
         print(f"请求处理时间: {end_time - start_time}")
         return response
-    return app
+
+    @app.middleware("http")
+    async def logging_middleware(request: Request, call_next):
+        message = f"{request.client.host}:{request.client.port} - {request.method} {request.url} - {time.strftime('%Y-%m-%d %H:%M:%S')}"
+        print(message)
+        response = await call_next(request)
+        return response
